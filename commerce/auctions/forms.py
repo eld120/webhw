38289f37bb953c189a7d95789576b8bbccc7bfa6
[eld120] <devteam@origma.io>
2021-03-08 22:02:21 -0600

    far too many changes - was a mistake, debugging

---
+++ commerce/auctions/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.forms.models import ModelForm
+from .models import Listings, Bids, Comments
+
+
+class ListingsForm(ModelForm):
+    class Meta:
+        model = Listings
+        fields = [ 'title', 'description', 'active', 'start_price', 'auction_length']
+
+    # title = forms.CharField(title='Title', max_length=150)
+    # description = forms.Textarea(max_length=500)
+    # start_price = forms.IntegerField()
+    # auction_length = forms.DateTimeField()
+
+# class BidForm(ModelForm):
+#     class Meta:
+#         model = Bids
+#         fields = ['listing_id', 'listing_bid']
+
+# class CommentForm(ModelForm):
+#     class Meta:
+#         model = Comments
+#         fields = ['listing_id', 'text']
